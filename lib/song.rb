bd05c9ff033b1047865604a5dcc3ca25014d7490
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-05-25 17:14:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,40 +1,4 @@
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
-
-  def self.create
-  song=Song.new
-  song.save
-  song
-  end
-
-  def self.new_by_name(new_name)
-    song=Song.new
-    song.name=new_name
-    song
-  end
-
-  def self.create_by_name(new_name)
-    song=self.create
-    song.name=new_name
-    song
-  end
-
-  def self.find_by_name(new_name)
-    self.all.find_by(new_name)
-  end
-
-  def self.find_or_create_by_name(new_name)
-
-  end
-
+  
 
 end
