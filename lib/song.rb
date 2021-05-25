3647ca406945effe2e9a2c39f82c6fdcd60d8482
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-05-25 17:40:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,27 +2,24 @@ class Song
 attr_accessor :name
 @@all=[]
 
-  def initialize(name=nil)
-    @name=name
-  end
-
   def self.all
     @@all
   end
 
   def self.create
     song=self.new
-    @@all<<song
+    self.all<<song
   end
 
   def self.new_by_name(name)
-    song=self.new(name)
+    song=self.new
+    song.name=name
   end
 
   def self.create_by_name(name)
     song=self.new
     song.name=name
-    song.save
+    self.all<<song
   end
 
 end
